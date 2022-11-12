a0130b1b86a94d1e2624824e6a1cef2eee0ce075
collinkan <collin.kan@gmail.com>
2022-11-11 22:37:15 -0500

    create navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="navbar-container">
+        $plit Cash
+    </div>
+</template>
+
+<script lang="ts">
+export default{
+}
+</script>
+
+<style scoped>
+.navbar-container{
+    z-index: 1;
+    width: 100%;
+    height: 10vh;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    background-color: aquamarine;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div >
+    <NavBar/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    NavBar,
   },
 })
 export default class Home extends Vue {}
