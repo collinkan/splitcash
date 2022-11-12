10a3f071f832cd5172b21c36aece1c9339780c16
collinkan <collin.kan@gmail.com>
2022-11-12 03:02:14 -0500

    implement bottom menu

---
+++ src/components/BottomMenu.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="menu-container">
+        <div id="bottom-menu">
+            <Button icon="pi pi-list" class="p-button-text" @click="$router.push('/bills')" />
+            <Button icon="pi pi-plus" class="p-button-text" @click="$router.push('/create')" />
+            <Button icon="pi pi-users" class="p-button-text" @click="$router.push('/friends')" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default{
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+<style scoped>
+.menu-container{
+    z-index: 1;
+    width: 100%; 
+    height: 15vh; 
+    position: fixed; 
+    bottom: 0;
+    padding: 10vh 3vh 10vh 3vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#bottom-menu{
+    width: 75vmax;
+    height: inherit;
+    border-radius: 1.5vmax;
+
+    -moz-user-select: -moz-none;
+    -khtml-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: rgb(22, 44, 88);
+}
+</style>
