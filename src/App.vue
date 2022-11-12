475fa9b36b956d32fda0fe06ba0727e8c1ec901c
collinkan <collin.kan@gmail.com>
2022-11-12 01:01:01 -0500

    add imports for PrimeVue

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,9 @@
 </template>
 
 <style>
-* { margin: 0 !important; }
 
 body{
-  background-color: rgb(44, 40, 44);
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-color: rgb(4, 13, 25);
+  margin: 0 !important; 
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,19 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/lara-dark-teal/theme.css'
+import 'primeicons/primeicons.css';
+import Button from 'primevue/button'
+import Sidebar from 'primevue/sidebar'
+import Avatar from 'primevue/avatar'
+
+const app = createApp(App)
+app
+    .use(router)
+    .use(PrimeVue)
+    .mount('#app')
+
+app.component('Button', Button)
+app.component('Sidebar', Sidebar)
+app.component('Avatar', Avatar)
